02d99bbc76a2c243c04b00920c5f798b1d5c5636
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-06-14 21:03:42 -0500

    Commit_06-14-22

---
+++ Python_Core/Ultimate_Element_List.py
@@ -0,0 +1,12 @@
+# List Slices
+
+
+# Write a program that takes a list as input and outputs the last element of that list.
+
+# The input list can be of any size.
+
+x = input()
+elements = x.split(" ")
+# lista_prueba= ['6', '45', '8', '99', '63', '341', '89462', '132', '165', '165']
+print (elements[-1])
+#your code goes here

6f878e2c26de453bf0b797d210c26c945b21dad7
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-06-16 22:06:44 -0500

    Commit_16-06

---
+++ Python_Core/Tabla 2.py
@@ -0,0 +1,17 @@
+import xdrlib
+
+
+nums = [i*2 for i in range(10)]
+
+print(nums)
+
+
+
+
+for x in nums:
+  
+  print("2 x",x)
+  # print(itera)
+
+
+ 
---
+++ Python_Core/multiplos_3.py
@@ -0,0 +1,2 @@
+a = [i for i in range(20) if i%3==0]
+print(a)

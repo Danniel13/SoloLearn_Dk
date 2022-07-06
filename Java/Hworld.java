ae49ddc697397b4c796e514418cdf0337a0b8da7
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-07-05 21:49:00 -0500

    Commit 7-05-22

---
+++ Java/Hworld.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class Hworld {
+  
+}
+class MyClass {
+  public static void main(String[ ] args) {
+    System.out.println("HWorld");
+  }
+}
---
+++ Java/Javaisfun.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class Javaisfun {
+  public static void main(String[] args) {
+    
+    System.out.println("Java is fun");
+  }
+}

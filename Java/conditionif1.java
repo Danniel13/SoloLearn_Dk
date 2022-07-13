b1e35fce2b0cb7886eb178bad27c7c3c47e1e9ff
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-07-12 23:23:30 -0500

    commitafan

---
+++ Java/conditionif1.java
@@ -0,0 +1,16 @@
+ package Java;
+
+import java.util.Scanner;
+
+public class conditionif1 {
+public static void main(String[] args) {
+  Scanner scanner = new Scanner(System.in);
+       int price = scanner.nextInt();
+       //complete the code
+       if (price <= 12000) {
+        System.out.println("yes");
+           
+       }
+}
+  
+}
